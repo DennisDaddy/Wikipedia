d07aa10ffd1ab63ff7755f2085cc61e708cdd528
DennisDaddy <softteez95@gmail.com>
2019-03-14 09:13:46 +0300

    Add favorite and history repository

--- app/src/main/java/org/dennis/wikipedia/activities/repositories/ArticleDatabaseOpenHelper.kt
+++ app/src/main/java/org/dennis/wikipedia/activities/repositories/ArticleDatabaseOpenHelper.kt
@@ -1,4 +1,27 @@
 package org.dennis.wikipedia.activities.repositories
 
-class ArticleDatabaseOpenHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+class ArticleDatabaseOpenHelper(context: Context) :ManagedSQLiteOpenHelper(context, "ArticlesDatabase.db", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        // define the tables in this database
+        db?.createTable("Favorites", true,
+                "id" to INTEGER + PRIMARY_KEY,
+                "title" to TEXT,
+                "url" to TEXT,
+                "thumbnailJson" to TEXT)
+
+        db?.createTable("History", true,
+                "id" to INTEGER + PRIMARY_KEY,
+                "title" to TEXT,
+                "url" to TEXT,
+                "thumbnailJson" to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        // Use to upgrade the schema of the database if needed
+    }
+
 }
---
+++ app/src/main/java/org/dennis/wikipedia/activities/repositories/HistoryRepository.kt
@@ -0,0 +1,45 @@
+package org.dennis.wikipedia.activities.repositories
+
+import com.google.gson.Gson
+import org.dennis.wikipedia.activities.models.WikiPage
+import org.dennis.wikipedia.activities.models.WikiThumbnail
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.rowParser
+
+class HistoryRepository(val databaseHelper: ArticleDatabaseOpenHelper) {
+    private val TABLE_NAME: String = "History"
+
+    fun addHistory(page: WikiPage){
+        databaseHelper.use {
+            insert(TABLE_NAME,
+                    "id" to page.pageid,
+                    "title" to page.title,
+                    "url" to page.fullurl,
+                    "thumbnailJson" to Gson().toJson(page.thumbnail))
+        }
+    }
+
+    fun removePageById(pageId: Int){
+        databaseHelper.use {
+            delete(TABLE_NAME, "id = {pageId}", "pageId" to pageId)
+        }
+    }
+
+
+    fun getAllHistory() : ArrayList<WikiPage>{
+        var pages = ArrayList<WikiPage>()
+
+        val articleRowParser = rowParser { id: Int, title: String, url: String, thumbnailJson: String ->
+            val page = WikiPage()
+            page.title = title
+            page.pageid = id
+            page.fullurl = url
+            page.thumbnail = Gson().fromJson(thumbnailJson, WikiThumbnail::class.java)
+
+            pages.add(page)
+        }
+
+        return pages
+    }
+}
